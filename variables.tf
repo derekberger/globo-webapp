d03a8325d9d721c7a07808810a78cf925c8d211c
Derek Berger <derekberger@dereks-mbp.attlocal.net>
2025-07-11 16:41:05 -0400

    feature: switch to secrets manager

--- variables.tf
+++ variables.tf
@@ -32,9 +32,9 @@ variable "instance_type" {
   default     = "t3.micro"
 }
 
-variable "api_key" {
+variable "api_key_secret_id" {
   type        = string
-  description = "(Required) API key for web app to talk to SaaS platform."
+  description = "(Required) Secret ID for the API key for web app to talk to SaaS platform."
 }
 
 variable "tfe_organization" {
@@ -45,6 +45,11 @@ variable "tfe_organization" {
 variable "tfe_workspace_name" {
   type        = string
   description = "(Required) Name of TFE workspace."
+}
+
+variable "ec2_role_name" {
+  type        = string
+  description = "(Required) Name of role to associate with EC2 instance profile."
 }
 
 variable "playbook_repository" {
